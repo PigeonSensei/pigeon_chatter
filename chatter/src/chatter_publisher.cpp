a51a0f7ec6d9263c1ad9d443fa1c1448e0dbae69
PigeonSensei <parkour5813@gmail.com>
2021-02-21 20:32:31 +0900

    -

---
+++ chatter/src/chatter_publisher.cpp
@@ -0,0 +1,63 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <time.h>
+
+int ReturnChatCommand(std::string cmd)
+{
+  if(cmd == "/exit")
+  {
+    std::cout << "exit the chatter_publisher_node" << std::endl;
+    return 0;
+  }
+  if(cmd == "/help")
+  {
+    std::cout << "/exit : exit the chatter_publisher_node" << std::endl;
+  }
+
+  return -1;
+}
+
+int main(int argc, char **argv)
+{
+  time_t time_now;
+  time_now = time(NULL);
+
+  std::string my_name;
+  std::string chat_message;
+
+  std::cout << "InPut your name : ";
+  std::cin >> my_name;
+
+  std::string time_stamp = std::to_string(time_now);
+  std::string node_name = "chatter_publisher_node_" + my_name + "_" + time_stamp;
+
+  ros::init(argc, argv, node_name);
+  ros::NodeHandle n;
+
+  ros::Publisher publisher_chatter = n.advertise<std_msgs::String>("chatter", 1000);
+
+  ros::Rate loop_rate(10);
+
+  while(ros::ok())
+  {
+    std_msgs::String msg;
+
+    std::cout << "send message : ";
+
+    std::cin >> chat_message;
+
+    std::cout << std::endl;
+
+    if(ReturnChatCommand(chat_message) == 0) break;
+
+    msg.data = my_name + " : " + chat_message;
+
+    publisher_chatter.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+   }
+
+  return 0;
+}
---
+++ chatter/src/chatter_subscriber.cpp
@@ -0,0 +1,50 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <time.h>
+#include <pwd.h>
+
+std::string GetTimeNow()
+{
+  time_t rawtime;
+  struct tm  *tm;
+  time(&rawtime);
+  tm = localtime(&rawtime);
+  std::string hour = std::to_string(tm->tm_hour);
+  std::string min = std::to_string(tm->tm_min);
+  std::string sec = std::to_string(tm->tm_sec);
+
+  std::string now_time = hour + ":" + min + ":" + sec;
+
+  return now_time;
+}
+
+void ChatterCallback(const std_msgs::String::ConstPtr& msg)
+{
+  std::cout << "[" << GetTimeNow() << "] : " << msg->data.c_str() << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+  uid_t user_id;
+  struct passwd *user_pw;
+
+  user_id = getuid();
+  user_pw = getpwuid(user_id);
+
+  std::string my_name = user_pw->pw_name;
+
+  time_t time_now;
+  time_now = time(NULL);
+
+  std::string time_stamp = std::to_string(time_now);
+  std::string node_name = "chatter_subscriber_node_" + my_name + "_" + time_stamp; 
+
+  ros::init(argc, argv, node_name);
+  ros::NodeHandle n;
+
+  ros::Subscriber subscriber_chatter = n.subscribe("chatter", 1000, ChatterCallback);
+
+  ros::spin();
+
+  return 0;
+}
