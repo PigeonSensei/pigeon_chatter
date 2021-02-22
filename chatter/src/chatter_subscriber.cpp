275f56e6da99f8d3d3faf840a7f4dc53c70196ca
PigeonSensei <parkour5813@gmail.com>
2021-02-22 09:01:16 +0900

    -

--- chatter/src/chatter_subscriber.cpp
+++ chatter/src/chatter_subscriber.cpp
@@ -20,7 +20,7 @@ std::string GetTimeNow()
 
 void ChatterCallback(const std_msgs::String::ConstPtr& msg)
 {
-  std::cout << "[" << GetTimeNow() << "] : " << msg->data.c_str() << std::endl;
+  std::cout << "[" << GetTimeNow() << "] " << msg->data.c_str() << std::endl;
 }
 
 int main(int argc, char **argv)
